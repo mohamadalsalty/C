ded38fec590e17ab71f89711370f921101e8c899
unknown <mohamadhazems@gmail.com>
2021-04-25 19:15:04 -0300

    update

---
+++ linear search implementation/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void){
+    int num1;
+    printf("Enter a number from 1 to 3: ");
+    scanf("%i", &num1);
+
+    int x[] = {1,2,3};
+    for(int i = 0; i < 3; i++){
+        if(x[i] == num1){
+            printf("%d, after = %d", x[i], i+1);
+        }
+    }
+}
