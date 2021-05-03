c4c1dfa5f9f53ae8dd47ba8ada40a398ac0b08b3
unknown <mohamadhazems@gmail.com>
2021-05-03 00:02:11 -0300

    update

--- cs50/linear_search.c
+++ cs50/linear_search.c
@@ -3,13 +3,13 @@
 
 int main()
 {
-    int rooms[] = {1,2,3,4,5,6,7,8,9};
+    int rooms[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     for (int x = 0; x < 9; x++)
     {
         // big o is o(n) = o(9)
         if (rooms[x] == 9)
         {
-            printf("%d looped %i", rooms[x], x+1);
+            printf("%d looped %i", rooms[x], x + 1);
         }
     }
     
